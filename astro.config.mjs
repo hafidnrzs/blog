1fbfed82cbf1fe3bff06e8b8cf068ff0e87515dc
Hafid Nur <hafidnurazis@gmail.com>
2026-02-28 23:51:19 +0700

    Remove nodejs integration to fix deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import node from '@astrojs/node';
-
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hafidnrzs.com',
   integrations: [mdx(), sitemap(), icon()],
-
-  adapter: node({
-    mode: 'standalone',
-  }),
 });
